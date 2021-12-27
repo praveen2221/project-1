a62ad68ef5f0d9e505c33d39b79a76926ffa015a
praveen2221 <praveenap2221@gmail.com>
2021-12-27 07:58:37 +0530

    done

---
+++ EmpwageBuilderUC1.java
@@ -0,0 +1,13 @@
+public class EmpwageBuilderUC1 {
+
+   public static void main(string[] arges) {
+        //constants
+        int IS_Full_Time = 1;
+        // computation
+        double empChack = Math.floor(Math.random() * 10) % 2);
+        if(empChack == IS_Full_Time)
+            System.out.println("Employee is Present");
+        else
+             System.out.println("Employee is Abscent");
+   }
+}
